57f6775b59b637f07b92ae2060315fca1dd34051
Emre Seymenler <60224571+eseymenler@users.noreply.github.com>
2024-07-03 13:45:04 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Person(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    birth_place = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.birth_place} - {self.birth_date}"
